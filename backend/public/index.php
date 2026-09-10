b2bb8a7ba4ce66ca3d020946bd2e50f1ad2f5ae4
Leonardo12122 <leo.de24mg@gmail.com>
2026-09-10 11:58:04 -0300

    estructura

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,9 +2,12 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Dotenv\Dotenv;
 
 
 require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
 $app = AppFactory::create();
 $app->get('/', function (Request $request, Response $response, $args) {
