12fc3505e3993e14f215719a028272406b2ab864
Leonardo12122 <leo.de24mg@gmail.com>
2026-09-11 15:33:23 -0300

    creacion de usuario verificada y comprobado

--- backend/src/Models/UserModel.php
+++ backend/src/Models/UserModel.php
@@ -1,4 +1,24 @@
 <?php
 namespace App\Models;
 
-use Slim\app;
+use PDO;
+use App\DB\DB;
+class UserModel{
+    public static function RegistrarUsuario($username,$nombre,$password){
+     $pdo = DB::conexion();
+     $sql = "INSERT INTO usuario (username, nombre, password) VALUES (?, ?, ?)";
+     $stmt = $pdo->prepare($sql);
+     return $stmt->execute([$username, $nombre, $password]);
+  
+    } 
+    public static function UsuarioExistente($username) {
+     $pdo = DB::conexion();
+     $sql = "SELECT 1 FROM usuario WHERE username = ?";
+     $stmt = $pdo->prepare($sql);
+     $stmt->execute([$username]);
+
+    return $stmt->fetch() ;
+}
+
+
+}
