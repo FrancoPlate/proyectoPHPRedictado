ecdcc1d7de9b3af44e1d2a73171fe3f4cfa0b6f4
Leonardo12122 <leo.de24mg@gmail.com>
2026-09-11 15:36:12 -0300

    actualizo index.php para que consuma a las rutas

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,19 +1,18 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use Dotenv\Dotenv;
-
 
 require __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
 
 $app = AppFactory::create();
+
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
 });
-(require __DIR__ . '/../src/routes/web.php')($app);
+
+(require __DIR__ . '/../src/Routes/web.php')($app);
 
 $app->run();
--- backend/src/Routes/web.php
+++ backend/src/Routes/web.php
@@ -1,4 +1,25 @@
 <?php
+
 namespace App\Routes;
 
-use Slim\app;
+use Slim\App;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Controllers\UserController;
+use App\Controllers\AutenticacionController;
+use App\Middleware\IsLoggedMiddleware;
+
+return function (App $app) {
+
+    $app->get('/saludo', function (Request $request, Response $response, $args) {
+        $response->getBody()->write("Hola, Slim funciona desde Routes!");
+        return $response;
+    });
+
+$app->post('/usuarios', [UserController::class, 'crearUsuario']);
+$app->post('/login', [AutenticacionController::class, 'IniciarSesion']);
+$app->post('/logout', [AutenticacionController::class, 'Logout'])->add(new IsLoggedMiddleware($app->getResponseFactory())); // para probar
+
+
+
+};
