7b95ab6608e1fd6855148a2ebc16e70fd73bf536
FrancoPlate <francoplatepaz@gmail.com>
2026-09-11 23:48:51 -0300

    agregado de rutas de chat
    uni las rutas de chat con los de user

--- backend/src/Routes/web.php
+++ backend/src/Routes/web.php
@@ -7,6 +7,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Controllers\UserController;
 use App\Controllers\AutenticacionController;
+use App\Controllers\ChatsController;
 use App\Middleware\IsLoggedMiddleware;
 
 return function (App $app) {
@@ -16,10 +17,18 @@ return function (App $app) {
         return $response;
     });
 
-$app->post('/usuarios', [UserController::class, 'crearUsuario']);
-$app->post('/login', [AutenticacionController::class, 'IniciarSesion']);
-$app->post('/logout', [AutenticacionController::class, 'Logout'])->add(new IsLoggedMiddleware($app->getResponseFactory())); // para probar
-$app->get('/usuarios/{user_id}', [UserController::class, 'ObtenerUsuario'])->add(new IsLoggedMiddleware($app->getResponseFactory()));;
-$app->put('/usuarios/{user_id}', [UserController::class, 'EditarUsuario'])->add(new IsLoggedMiddleware($app->getResponseFactory()));;
+    $app->post('/usuarios', [UserController::class, 'crearUsuario']);
+    $app->post('/login', [AutenticacionController::class, 'IniciarSesion']);
+    $app->post('/logout', [AutenticacionController::class, 'Logout'])->add(new IsLoggedMiddleware($app->getResponseFactory())); // para probar
+    $app->get('/usuarios/{user_id}', [UserController::class, 'ObtenerUsuario'])->add(new IsLoggedMiddleware($app->getResponseFactory()));
+    $app->put('/usuarios/{user_id}', [UserController::class, 'EditarUsuario'])->add(new IsLoggedMiddleware($app->getResponseFactory()));
+    
+    //----------Chat----------\\
+    $app->post('/chats/{user_id}', [ChatsController::class, 'newChat'])->add(IsLoggedMiddleware::class);
+    $app->get('/chats', [ChatsController::class, 'getChats'])->add(IsLoggedMiddleware::class);
+    $app->put('/chats/{user_id}', [ChatsController::class, 'putChat'])->add(IsLoggedMiddleware::class);
+    $app->get('/chats/{user_id}/historia/{quantity}', [ChatsController::class, 'getHistory'])->add(IsLoggedMiddleware::class);
+    
+
 
 };
